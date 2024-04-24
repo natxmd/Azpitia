89e90913012eeaf5def55ccb0b40c1f2a2232fa0
unknown <yurgen6661@gmail.com>
2024-04-24 16:41:14 -0500

    cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import CardGeneral from "./components/card/CardGeneral";
+
 export default function App() {
    return (
-      <div>
-         
+      <div className="">
+         <CardGeneral
+            imageUrl="/hero/img1.png"
+            title="CELEBRANDO EL DÍA DE LA MADRE"
+            description="Porque no hay mejor momento para reunir a la familia"
+            buttonText="QUIERO IR"
+         />
       </div>
    )
 }
---
+++ src/components/card/CardGeneral.tsx
@@ -0,0 +1,38 @@
+
+interface CardGeneralProps {
+    imageUrl: string;
+    title: string;
+    description: string;
+    buttonText: string;
+}
+
+const CardGeneral: React.FC<CardGeneralProps> = ({ imageUrl, title, description, buttonText }) => {
+    return (
+        <div className="h-[600px] min-w-[328px] relative">
+            <img
+                src={imageUrl}
+                alt=""
+                className="relative rounded-2xl object-cover w-full h-full"
+            />
+
+            <div className="absolute bottom-[21px] left-0 right-0 grid gap-[16px] items-center justify-center text-center  ">
+                <div className="">
+                    <h1 className="text-[35px] font-bold font-[Cinzel] text-[#D5E11D]">
+                        {title}
+                    </h1>
+                    <p className="text-white font-normal font-[Roboto Flex] text-[15px] text-center">
+                        {description}
+                    </p>
+                </div>
+                <button
+                    className="max-w-[146px] h-[48px] rounded-[16px] mx-auto px-[28px]
+                    bg-[#1A4D3E] text-[#D5E11D] border-2 border-[#D5E11D]">
+                    {buttonText}
+                </button>
+            </div>
+
+        </div>
+    );
+}
+
+export default CardGeneral;
